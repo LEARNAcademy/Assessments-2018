caf2d64d700803f0738f8a13a72856e00503c7d5
Jennifer Hallquist <jnhallquist@gmail.com>
2018-02-13 15:40:49 -0800

    Start work on js problems

--- Jennifer-Hallquist-Week-3.js
+++ Jennifer-Hallquist-Week-3.js
@@ -1,6 +1,18 @@
 // 1. Write a simple React component that simply prints "I am a component" to
 // the screen. Be sure to include all necessary imports, exports, etc...
 
+import React, { Component } from 'react';
+
+export default class Example extends Component {
+  render() {
+    return (
+      <p>
+        I am a component.
+      </p>
+    );
+  }
+}
+
 // 2. Practice With Loops: In React, we often use the map function to iterate
 // over an array held in state. To get more practice with this, write a vanilla
 // javascript for loop that outputs the same thing as the map function below.
@@ -14,15 +26,18 @@ stuffArray.map(function(el, i){
 
 // your for loop here, you can use the same stuffArray array
 
+for (let i = 0; i < stuffArray.length; i++) {
+  console.log(`${stuffArray[i]} is at index: ${i}`);
+}
 
 // 3. Basic sorting: Find the cheapest price -- you are given a list of
 // prices and need to find the signle lowest price from the array. Write a
 // function that takes in an array of numbers, and returns the lowest price
 
-var steal = function(){
+const steal = (array) => {
 
   // your logic here
-
-}
+  return Math.min(array);
+};
 
 4.
