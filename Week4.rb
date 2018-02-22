400c70d351692df1df43554b65d5cf5ff11ccbee
Nicolas D'Amico-Barbour <nico@Nicolass-Air.localdomain>
2018-02-22 08:05:10 -0800

    week 3 & 4

--- Week4.rb
+++ Week4.rb
@@ -2,27 +2,51 @@
 
 tempArray = [1, 2, 6, 9, 3, 21]
 
+
+
 # your ruby loop here
+
+tempArray.map!{|x| x*2}
+
+# i briefly considered doing a tempArray.each{} but then realized that would return the desired values but would not modify the array. Map also returns an array, but map! reassigns that array to the original object.
 
 #### 2. From all the built in Ruby methods we've seen in class this week, choose three that you think are particularly helpful and create examples to show how they work.
 
 # your three built in ruby methods
 
-#### 3. Look at this horrible ruby code, and fix it to be good ruby code. 
+.to_i & to_f
+
+"4".to_i = 4
+"4".to_f = 4.0
+
+#Often times when you are getting unput from a user, even if it's number form it is read by ruby as a string. by using .to_i the value is converted to an integer that can be operated on as a value. to_f does the same thing but allows for decimals to be used as well.
+
+downcase(!) and upcase (!)
+
+"AAAAAH".downcase = "aaaaah"
+"aaaaah".upcase = "AAAAAH"
+
+#these are probably not the MOST useful methods for strings, but coming from javascript they feel like a luxury, especially when having to manipulate user inputted information like names and whatnot
+
+flat_map
+
+[[4,4,4],4,[4,[4,[4]]],4,4].flat_map = [4,4,4,4,4,4,4,4,4]
+
+# this method called on an array will let you flatten the array so that all arrays inside the array will just become part of the array, multi-dimensional become one-dimensional
+
+#### 3. Look at this horrible ruby code, and fix it to be good ruby code.
 
 ``` ruby
 class example
   def initialize(day)
     @day=day
   end
-  
-  def Say_hi
-    if(day == "Friday"){
+
+  def say_hi
+    if @day == "Friday"
       puts "TGIF!"
-    }
-    elseif(day == "Monday"){
+    elsif @day == "Monday"
       puts "Its monday again"
-    }
     else
       puts "another day"
   end
