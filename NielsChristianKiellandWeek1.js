1bfabc7e197d6bee67517834b4f4a268478bf60a
Niels-Christian Kielland <nielschristian@Niels-Christians-MacBook-Pro.local>
2018-08-04 09:14:10 -0700

    finished Week 1 assessments js

--- NielsChristianKiellandWeek1.js
+++ NielsChristianKiellandWeek1.js
@@ -19,11 +19,39 @@ divisbleByThree(10);
 	// Include at least three properties of you
 	// and store your object in a variable with your name.
 
-var Niels = {
+function createPerson(name, spokenLangauges, pet){ return {
+	_name: name,
+	_spokenLangauges: spokenLangauges,
+	_pet: pet };
 }
+
+var Niels = createPerson("Niels", ["Norwegian", "English"],
+	{breed: "Australian Shepherd", name: "Hazel"});
+
+console.log(Niels);
+console.log(Object.values(Niels));
+console.log("My name is " + Niels._name + ". I speak two languages " + Niels._spokenLangauges + ". I have an " + Niels._pet.breed + " named " + Niels._pet.name + ".");
 
 // 3. Create an array with at least 4 items inside it.
 	// Show how to access two values from the array in two different ways.
+
+var superheroes = [{name: "Batman", secretIdentity: "Bruce Wayne"}, {name: "Flash", secretIdentity: "Wally West"},
+									 {name: "Green Lantern", secretIdentity: "John Stewart"}, {name: "Wonder Woman", secretIdentity: "Diana Prince"}];
+
+console.log(superheroes[0], superheroes[1]);
+console.log(superheroes[0].name + " " + superheroes[0].secretIdentity)
+
+function whatsInHalfTheArr(arr){
+	for (i = 0; i < ( arr.length / 2 ); i++){
+		for (key in arr[i]) {
+			console.log(arr[i][key]);
+		}
+	}
+}
+
+whatsInHalfTheArr(superheroes);
+
+
 
 // 4. Have the function AlphabetSoup(str) take the "str" parameter
 	// being passed and return a string with the letters in alphabetical order
@@ -35,19 +63,72 @@ var Niels = {
 
 function AlphabetSoup(str) {
 
-    // your code goes here
+	var str = str.toLowerCase();
+	var strArr = [];
 
+	for(i = 0; i < str.length; i++){
+		strArr.push(str.charAt(i));
+	}
+
+	str = strArr.sort().join("");
 	return str;
+
 }
+
+function AlphabetSoup2(str) {
+	return str.split('').sort((a,b) => a.localeCompare(b, { ignorePunctuation: true })).join('');
+}
+
+console.log(AlphabetSoup("hooplah"));
+console.log(AlphabetSoup2("HooPlah"));
 
 // keep this function call here
 
-AlphabetSoup(readline());
+// AlphabetSoup(readline());
 
-// 5. Given the arrays below, use a for loop to print one value from each array concatenated together. How would your code need to change to accomodate arrays of different lengths?
+// 5. Given the arrays below, use a for loop to print one value from
+	// each array concatenated together. How would your code need to
+	// change to accomodate arrays of different lengths?
 
-var nums = [1, 5, 88, 2, 5, 42, 57, 101]
+var nums = [1, 5, 88, 2, 5, 42, 57, 101];
 
 var nouns = ["ducks", "telephone booth", "the enterprise", "robots", "amazon", "eraser", "zafod", "a"]
+
+function numsOfNouns(arr1, arr2){
+	for (i = 0; i < arr1.length; i++){
+		console.log(arr1[i] + " " + arr2[i]);
+	}
+	return;
+}
+
+numsOfNouns(nums, nouns);
+
+var nums2 = [1, 5, 88, 2, 5, 42, 57, 101, 1000, 54, 89];
+
+var nouns2 = ["ducks", "telephone booth", "the enterprise", "robots", "amazon", "eraser", "zafod", "a"]
+
+function numsOfNouns2(arr1, arr2){
+	for (i = 0; i < arr1.length || i < arr2.length; i++){
+		console.log(arr1[i] + " " + arr2[i]);
+	}
+	return;
+}
+
+numsOfNouns2(nums2, nouns2);
+
+var nums3 = [1, 5, 88, 2, 5, 42, 57, 101, 1000, 54, 89];
+
+var nouns3 = ["ducks", "telephone booth", "the enterprise", "robots", "amazon", "eraser", "zafod", "a", "banana", "bird"]
+
+function numsOfNouns3(arr1, arr2){
+	for (i = 0; i < arr1.length || i < arr2.length; i++){
+		if (arr1[i] && arr2[i]){
+			console.log(arr1[i] + " " + arr2[i]);
+		}
+	}
+	return;
+}
+
+numsOfNouns3(nums3, nouns3);
 
 // output of the first function should be: "1 ducks"
