5fac815b61f7914d42b877cb62d3720e5e08485f
AJ Magracia <ajmagracia@gmail.com>
2018-08-27 12:00:44 -0700

    minor update

--- Week-3.js
+++ Week-3.js
@@ -1,4 +1,4 @@
-1. Write a simple React component that simply prints "I am a component" to the screen. Be sure to include all necessary imports, exports, etc...
+// 1. Write a simple React component that simply prints "I am a component" to the screen. Be sure to include all necessary imports, exports, etc...
 
 import React, { Component } from 'react'
 class Label extends Component {
@@ -15,7 +15,7 @@ export default Label
 
 
 
-2. Practice With Loops: In React, we often use the map function to iterate over an array held in state. To get more practice with this, write a vanilla javascript for loop that outputs the same thing as the map function below.
+// 2. Practice With Loops: In React, we often use the map function to iterate over an array held in state. To get more practice with this, write a vanilla javascript for loop that outputs the same thing as the map function below.
 
 
 var stuffArray = [42, "Arthur Dent", 1978, "Zaphod", "Vogon", "Marvin, the Paranoid Android"]
@@ -29,15 +29,16 @@ for (let i = 0; i < stuffArray.length; i++) {
   console.log(`${stuffArray[i]} is at index: ${i}`)
 }
 
-3. Basic sorting: Find the cheapest price -- you are given a list of prices and need to find the signle lowest price from the array. Write a function that takes in an array of numbers, and returns the lowest price
+// 3. Basic sorting: Find the cheapest price -- you are given a list of prices and need to find the signle lowest price from the array. Write a function that takes in an array of numbers, and returns the lowest price
 
 var steal = function(arr){
 
   // your logic here
-return Math.min(arr)
+return Math.min(...arr)
 }
 
 var steal2 = function(arr){
 sorted = arr.sort()
 return sorted[0]
 }
+console.log (steal([5,3,7,1,6]))
