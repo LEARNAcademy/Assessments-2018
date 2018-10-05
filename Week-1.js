0f22c2ffd1d30c1b62dd723154c69b1523eb3623
byeondtjr <beyondtjr@gmail.com>
2018-10-05 11:48:50 -0700

    My response to 1/Week1.

--- Week-1.js
+++ Week-1.js
@@ -1,24 +1,33 @@
 // 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three". If it is not, print that the number "is not divisble by three".
 
+function anonymous(number) {
+	if (number % 3 === 0) {
+		console.log(number + " is divisible by three.");
+	} else {
+		console.log(number + " is not divisble by three");
+	}
+}
+anonymous(9);
+
 // 2. Write about yourself using an object. Include at least three properties of you and store your object in a variable with your name.
 
 // 3. Create an array with at least 4 items inside it. Show how to access two values from the array in two different ways.
 
-// 4. Have the function AlphabetSoup(str) take the "str" parameter being passed and return a string with the letters in alphabetical order (ie. hello becomes ehllo). Assume no numbers or punctuation symbols will not be included in the parameter. 
+// 4. Have the function AlphabetSoup(str) take the "str" parameter being passed and return a string with the letters in alphabetical order (ie. hello becomes ehllo). Assume no numbers or punctuation symbols will not be included in the parameter.
 
 // Input:"hooplah"
 // Output:"ahhloop"
 
 function AlphabetSoup(str) {
 
-    // your code goes here  
-	
-	return str; 
+    // your code goes here
+
+	return str;
 }
 
-// keep this function call here 
+// keep this function call here
 
-AlphabetSoup(readline());   
+AlphabetSoup(readline());
 
 // 5. Given the arrays below, use a for loop to print one value from each array concatenated together. How would your code need to change to accomodate arrays of different lengths?
 
