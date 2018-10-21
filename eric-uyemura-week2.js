b3df906893a662851c1975194506470d75f93fec
euyemura <ea.uyemura@gmail.com>
2018-10-21 15:34:47 -0700

    second week js

--- eric-uyemura-week2.js
+++ eric-uyemura-week2.js
@@ -1,14 +1,12 @@
 // 1. Write an anonymous function that takes one argument of type array and returns the contents of that array in reverse order.
 
-// test array: var testCase = ["one", "two", "three", "four"]
+var testCase = ["one", "two", "three", "four"]
 
-var reverseArray(list) {
-  // ... your code here
-}
+var reverseArray = (list) =>  list.reverse()
 
-reverseArray(test)
-
-// expected output: ["four", "three", "two", "one"]
+console.log(testCase)
+let newArr = reverseArray(testCase)
+console.log(newArr)
 
 // 2. Given the object below, fill in how to find specific information:
 
@@ -23,22 +21,31 @@ var bicycle = {
 }
 
 // Log the type of bicycle:
-console.log()
+console.log(bicycle.type)
 
 // Log just the bell from the list of gear
-console.log()
+console.log(bicycle.gear[2])
 
 // Log the correct PSI for the tires
-console.log()
+console.log(bicycle.wheels.specs[2])
 
 // 3. Write a function that takes two arguments, a string and an individual letter. The function should return a count of how many times the letter appears in the string.
 
 // examples:
 
-countLetter("Hello World", "l")
+// function howMany(str, letter) {
+// 	let regex = /letter/g
+// 	return str.match(regex)
+// }
+
+function howMany(str, letter) {
+	return (str.split(letter).length) -1
+}
+
+console.log(howMany("Hello World", "l"))
 
 // should return => 3
 
-countLetter("Hello World", "z")
+console.log(howMany("Hello World", "z"))
 
 // should return => 0
