9e41accd0d7b8f191f0d5be717ebe5d9d499ef08
janwrx <janzosa@gmail.com>
2018-11-04 12:35:48 -0800

    Week 4 finished

--- Week-4.rb
+++ Week-4.rb
@@ -2,20 +2,34 @@
 
 tempArray = [1, 2, 6, 9, 3, 21]
 
-# your ruby loop here
+def times_two(tempArray)
+  empty_arr = []
+    arr.each do |el|
+      empty_arr << el * 2
+end
 
 #### 2. From all the built in Ruby methods we've seen in class this week, choose three that you think are particularly helpful and create examples to show how they work.
 
-# your three built in ruby methods
+example_methods = [1, 2, 3, 4, 5]
+def add_one.map(array)
+  array.map{ |x| x + 1 }
+end #adds 1 to each element in array
 
-#### 3. Look at this horrible ruby code, and fix it to be good ruby code. 
+example_methods = [5, 20, 4, 15]
+p example_methods.select { |numbers| numbers > 9 } #will only print numbers greater than 9
+
+first_name = "janice"
+last_name = "zosa"
+p first_name.upcase #will print out user answer for first name first letter capitalized
+
+#### 3. Look at this horrible ruby code, and fix it to be good ruby code.
 
 ``` ruby
 class example
   def initialize(day)
     @day=day
   end
-  
+
   def Say_hi
     if(day == "Friday"){
       puts "TGIF!"
@@ -28,3 +42,20 @@ class example
   end
 end
 ```
+
+class Example
+  def initialize (day)
+    @day = day
+  end
+
+  def say_hi
+    if(day === "Friday"){
+      puts "TGIF!"
+    }
+    elsif(day === "Monday"){
+      puts "It's Monday again."
+    }
+    else
+      puts "Another day"
+  end
+end
