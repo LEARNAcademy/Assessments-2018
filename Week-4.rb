5cbb62ea556a3bb793326cf5b7499f0b10abd57e
Kristen McCloud <krisnmccloud@gmail.com>
2018-11-04 18:51:29 -0800

    week 4 rb

--- Week-4.rb
+++ Week-4.rb
@@ -2,27 +2,38 @@
 
 tempArray = [1, 2, 6, 9, 3, 21]
 
-# your ruby loop here
+tempArray.each do |el|
+  tempArray *= 2
+  print tempArray
+end
 
 #### 2. From all the built in Ruby methods we've seen in class this week, choose three that you think are particularly helpful and create examples to show how they work.
 
-# your three built in ruby methods
+myArr = ["h", "E", "l", "L", "o"]
 
-#### 3. Look at this horrible ruby code, and fix it to be good ruby code. 
+p myArr.to_s # HElLo
+
+p myArr.downcase # hello
+
+p myArr.include?("e") # true
+
+
+
+#### 3. Look at this horrible ruby code, and fix it to be good ruby code.
 
 ``` ruby
 class example
   def initialize(day)
-    @day=day
+    @day = day
   end
-  
-  def Say_hi
-    if(day == "Friday"){
+
+  def say_hi
+    if(day == "Friday")
       puts "TGIF!"
-    }
-    elseif(day == "Monday"){
+
+    elsif(day == "Monday")
       puts "Its monday again"
-    }
+    
     else
       puts "another day"
   end
